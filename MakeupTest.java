be4dc0377681557eb893c71c5892ff86222b6b83
Dr Hope Mauwa <Hope.Mauwa@ump.ac.za>
2024-05-14 09:34:15 +0200

    Commit after updating the file

--- MakeupTest.java
+++ MakeupTest.java
@@ -1,5 +1,7 @@
 public class MakeupTest{
 	public static void main(String[] args){
 		System.out.println("This is just for a makeup test");
+		System.out.println("Updating the file before pushing it to the remote repo");
+		System.out.println("I hope this works well without any errors");
 	}
 }
